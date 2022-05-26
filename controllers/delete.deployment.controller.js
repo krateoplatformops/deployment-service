60896f2b1a20bd0fb66201f6f31613dadd46dfac
mauro sala <mauro.sala@gmail.com>
2022-05-26 14:44:32 +0200

    feat(delete): call to kube bridge

--- controllers/delete.deployment.controller.js
+++ controllers/delete.deployment.controller.js
@@ -2,27 +2,38 @@ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 const Deployment = mongoose.model('Deployment')
+const yaml = require('js-yaml')
 
 router.delete('/:id', async (req, res, next) => {
   try {
-    Deployment.deleteOne({ _id: req.params.id })
-      .then((doc) => {
-        if (doc.deletedCount === 0) {
-          res
-            .status(404)
-            .json({ message: `Deployment with id ${req.params.id} not found` })
-        } else {
-          // TODO: call kube bridge to delete the deployment
-          res
-            .status(200)
-            .json({ message: `Deployment with id ${req.params.id} deleted` })
-        }
-      })
-      .catch(() => {
+    Deployment.findByIdAndDelete(req.params.id, async (err, deployment) => {
+      if (err) {
+        next(err)
+      } else if (!deployment) {
         res.status(404).json({
           message: `Deployment with id ${req.params.id} not found and cannot be deleted`
         })
-      })
+      } else {
+        // call kube bridge
+        await axios.delete(
+          uriHelpers.concatUrl([envConstants.BRIDGE_URI, 'apply']),
+          {
+            encoding: 'base64',
+            claim: stringHelpers.to64(yaml.dump(deployment.claim)),
+            package: stringHelpers.to64(yaml.dump(deployment.package))
+          },
+          {
+            headers: {
+              'X-Deployment-Id': deployment._id
+            }
+          }
+        )
+        // response
+        res
+          .status(200)
+          .json({ message: `Deployment with id ${req.params.id} deleted` })
+      }
+    })
   } catch (error) {
     next(error)
   }
