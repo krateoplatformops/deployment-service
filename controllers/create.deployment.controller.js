482d17873a6e691d59148c4472ed9d1cc63c6026
mauro sala <mauro.sala@gmail.com>
2023-01-16 15:06:53 +0100

    fix(create): preparsing

--- controllers/create.deployment.controller.js
+++ controllers/create.deployment.controller.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const axios = require('axios')
 const Mustache = require('mustache')
+const yaml = require('js-yaml')
 const k8s = require('@kubernetes/client-node')
 
 const uriHelpers = require('../service-library/helpers/uri.helpers')
@@ -135,7 +136,7 @@ router.post('/', async (req, res, next) => {
     // }
     // logger.debug(newValues)
 
-    const claim = Mustache.render(claimString, placeholder)
+    const claim = yaml.load(Mustache.render(claimString, placeholder))
 
     logger.debug(claim)
 
