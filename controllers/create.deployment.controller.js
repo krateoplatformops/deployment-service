17143536bf3c32641859ef84161a7ca27cb27dcc
Diego Braga <diego.braga86@gmail.com>
2023-07-04 18:13:55 +0200

    Adding more verbose log

--- controllers/create.deployment.controller.js
+++ controllers/create.deployment.controller.js
@@ -58,7 +58,10 @@ router.post('/', async (req, res, next) => {
     logger.debug('endpointName=', endpointName)
     logger.debug('endpoint before switch (1)=', endpoint)
     logger.debug('endpoint before switch (2)=', JSON.stringify(endpoint))
-    logger.debug('endpoint.metadata.type=', endpoint.metadata.type)
+    logger.debug('endpoint.metadata.type (1)=', endpoint.metadata.type)
+    logger.debug('endpoint.metadata.type (2)=', JSON.stringify(endpoint.metadata.type))
+    logger.debug(JSON.stringify(endpoint.metadata.type))
+    logger.debug('|- endpoint.metadata.type (2)')
 
     let path = null
     switch (endpoint.metadata.type) {
