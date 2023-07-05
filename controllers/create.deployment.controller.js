da83a827a34fbec6dc1417924c1bea855492f69a
Diego Braga <diego.braga86@gmail.com>
2023-07-05 19:20:33 +0200

    Adding more verbose log

--- controllers/create.deployment.controller.js
+++ controllers/create.deployment.controller.js
@@ -104,7 +104,7 @@ router.post('/', async (req, res, next) => {
         break
       case 'azuredevops':
         path = [pathList[0], pathList[1], pathList[3].split('?')[0]]
-        const queryparameter = pathList[3].split('?')[1] + '/' + pathList[4]
+        const queryparameter = /* pathList[3].split('?')[1] + */'/' + pathList[4]
 
         logger.debug('<- path')
         logger.debug(JSON.stringify(path))
