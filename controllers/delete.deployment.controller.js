f8292c1ec7e2947ec67d4d27c9ce19ab8025e577
Diego Braga <diego.braga86@gmail.com>
2024-03-27 08:43:24 +0100

    fix: add log to delete deployment (#4)

--- controllers/delete.deployment.controller.js
+++ controllers/delete.deployment.controller.js
@@ -4,6 +4,19 @@ const k8sHelpers = require('../service-library/helpers/k8s.helpers')
 
 router.delete('/', async (req, res, next) => {
   try {
+
+    logger.debug('<- req')
+    logger.debug(JSON.stringify(req))
+    logger.debug('<- req')
+
+    logger.debug('<- res')
+    logger.debug(JSON.stringify(res))
+    logger.debug('<- res')
+
+    logger.debug('<- next')
+    logger.debug(JSON.stringify(next))
+    logger.debug('<- next')
+
     await k8sHelpers.deleteByName(
       `/apis/${req.body.apiVersion}/${req.body.kind.toLowerCase()}`,
       req.body.name.toLowerCase()
